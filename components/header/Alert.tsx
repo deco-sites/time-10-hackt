62bc6ca37b81a584cd7ebeed8e607a317ecebf37
rayanemelo <raymeloc@gmail.com>
2023-03-17 22:02:46 -0300

    chore: alert

--- components/header/Alert.tsx
+++ components/header/Alert.tsx
@@ -12,12 +12,17 @@ export interface Props {
   interval?: number;
 }
 
-function Alert({ alerts = [], interval = 5 }: Props) {
-  const id = useId();
+const location = "São Paulo-SP"
+
+function Alert() {
 
   return (
-    <div id={id}>
-      <Slider class="bg-badge gap-6 scrollbar-none">
+    <div class="bg-[#040491] h-8">
+      <div>
+        <Text class="text-[#fff] text-sm">Ofertas para <span class="text-[#00CF80]">{location}</span></Text>
+        <button class="bg-[#00CF80] text-[#040491] ml-1 rounded-full px-2 text-sm">alterar</button>
+      </div>
+    {/*   <Slider class="bg-badge gap-6 scrollbar-none">
         {alerts.map((alert) => (
           <Text
             class="flex justify-center items-center w-screen h-[38px]"
@@ -25,11 +30,12 @@ function Alert({ alerts = [], interval = 5 }: Props) {
             tone="default-inverse"
           >
             {alert}
+            salkjsikakosk
           </Text>
         ))}
       </Slider>
 
-      <SliderControllerJS rootId={id} interval={interval && interval * 1e3} />
+      <SliderControllerJS rootId={id} interval={interval && interval * 1e3} /> */}
     </div>
   );
 }
