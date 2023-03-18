47e4de96d89fc638b67980df56fb09a34f46c4e4
rayanemelo <raymeloc@gmail.com>
2023-03-18 13:23:43 -0300

    feat: header

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -7,6 +7,8 @@ import { navbarHeight } from "./constants.ts";
 import type { INavItem } from "./NavItem.tsx";
 import type { Props as SearchbarProps } from "$store/components/search/Searchbar.tsx";
 import HeaderSearchMenu from "$store/islands/HeaderSearchMenu.tsx";
+import SearchInput from "./SearchInput.tsx";
+import ButtonLogin from "./ButtonLogin.tsx";
 
 function Navbar({ items, searchbar }: {
   items: INavItem[];
@@ -35,27 +37,40 @@ function Navbar({ items, searchbar }: {
       </div>
 
       {/* Desktop Version */}
-      <div class="hidden md:flex flex-row justify-between items-center border-b-1 border-default w-full pl-2 pr-3">
-        <div class="flex-none w-44">
-          <a href="/" aria-label="Store logo" class="block px-4 py-3 w-[160px]">
-            <Icon id="Logo" width={126} height={16} />
-          </a>
-        </div>
-        <div class="flex-auto flex justify-center">
-          {items.map((item) => <NavItem item={item} />)}
-        </div>
-        <div class="flex-none w-44 flex items-center justify-end gap-2">
-          <HeaderButton variant="search" />
-          <HeaderSearchMenu searchbar={searchbar} />
-          <Button
-            as="a"
-            variant="icon"
-            href="/login"
-            aria-label="Log in"
-          >
-            <Icon id="User" width={20} height={20} strokeWidth={0.4} />
-          </Button>
-          <HeaderButton variant="cart" />
+      <div class="hidden md:flex flex-col justify-between items-center border-b-1 border-default w-full pl-2 pr-3 bg-[#040491] ">
+        <div class="flex flex-col  max-w-[1200px] m-auto w-full justify-between">
+          <div class="py-3 flex justify-between">
+            <img
+              src="https://novomundo.vtexassets.com/arquivos/v2-logo-novo-mundo.png?v=20230314100853"
+              alt="Novo Mundo"
+              class="block w-[300px] mr-6 object-contain	"
+            />
+            <SearchInput />
+            <ButtonLogin />
+            <div class="pl-8">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="#fff"
+                className="w-10 h-10"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+            </div>
+          </div>
+
+          <div class="flex">
+            <div class="flex-auto flex justify-center">
+              {items.map((item) => <NavItem item={item} />)}
+            </div>
+           
+          </div>
         </div>
       </div>
     </>
